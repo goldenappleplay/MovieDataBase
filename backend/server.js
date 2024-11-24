0adb9df930cc6c86a8233f73fda0e77d6c521a74
Kosta Ivanov <kostasvetlozarov@gmail.com>
2024-11-24 23:53:46 +0200

    Set up MongoDB connection and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 // server.js
 
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
